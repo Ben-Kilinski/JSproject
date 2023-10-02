70c481d943b5f4a33042520c714bcaf7592dddc7
Ben-Kilinski <ben.kilinski@gmail.com>
2023-10-02 14:24:50 +0300

    searchpage

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 
-const moviesData = getMovies()
-
 function getMovies() {
-    fetch("https://moviesminidatabase.p.rapidapi.com/movie/id/%7Bmovie_id%7D/cast/")
+    
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTQxZDY0ZTQyOWRiNGFiNDFjMDU1NGE4MGY0MDkwZiIsInN1YiI6IjY1MWE4NjEyZWE4NGM3MDEwYzEzOGFlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QmBsb0LIN7ieO3P2dJwuiUZw74HQHwrhlTXcaVKNI7w'
+        }
+    }
+
+    fetch("https://api.themoviedb.org/3/authentication", options)
      
         .then(response => {
             console.log(response); 
@@ -16,3 +23,8 @@ function getMovies() {
 
         .catch(error => console.log(error))
 }
+
+
+
+  
+  
