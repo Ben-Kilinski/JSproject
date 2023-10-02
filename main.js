58f96d5ad2c8c93ba7e376d15e1dffde4ef5770f
Ben-Kilinski <ben.kilinski@gmail.com>
2023-10-02 12:16:30 +0300

    navBAr

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-function getJoke(){
-    
-    fetch("https://api.chucknorris.io/jokes/random")
+
+const moviesData = getMovies()
+
+function getMovies() {
+    fetch("https://moviesminidatabase.p.rapidapi.com/movie/id/%7Bmovie_id%7D/cast/")
      
         .then(response => {
             console.log(response); 
@@ -8,25 +10,9 @@ function getJoke(){
         })
 
         .then(data => {
-            const container = document.getElementById("container");
-            container.innerHTML = `<p>${data.value}</p>`
-})}
+            const movieList = document.getElementById("movieList");
+            movieList.innerHTML = `<p>${data.value}</p>`
+        })
 
-const jokeButton = document.getElementById("jokesBtn")
-jokeButton.addEventListener("click", getJoke)
-
-//--------------------------------------------------------------------------------   
-
-
-const fetchWeather = () => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric&appid=7daaf87b350b92f5266dbd06b61ab82e`
-    fetch(url)
-    .then(res => res.json())
-    .then(data =>{ console.log(data)
-        document.body.innerHTML = `<p>${data.main.temp}</p>`
-    })
-    .catch(error => console.log(error))
+        .catch(error => console.log(error))
 }
-const confirmBtn = document.getElementById("confirm")
-confirmBtn.addEventListener("click", fetchWeather)
-    
