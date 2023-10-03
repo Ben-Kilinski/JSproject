e43db58edef539e5c4aa7dec715e05f2ebc79f17
Ben-Kilinski <ben.kilinski@gmail.com>
2023-10-03 10:19:39 +0300

    searchPage

--- main.js
+++ main.js
@@ -1,30 +1,20 @@
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTQxZDY0ZTQyOWRiNGFiNDFjMDU1NGE4MGY0MDkwZiIsInN1YiI6IjY1MWE4NjEyZWE4NGM3MDEwYzEzOGFlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QmBsb0LIN7ieO3P2dJwuiUZw74HQHwrhlTXcaVKNI7w'
+    }
+  };
+
+const api_url = "https://www.themoviedb.org/movieapi_key=3141d64e429db4ab41c0554a80f4090f"
 
 function getMovies() {
-    
-    const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTQxZDY0ZTQyOWRiNGFiNDFjMDU1NGE4MGY0MDkwZiIsInN1YiI6IjY1MWE4NjEyZWE4NGM3MDEwYzEzOGFlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QmBsb0LIN7ieO3P2dJwuiUZw74HQHwrhlTXcaVKNI7w'
-        }
-    }
-
-    fetch("https://api.themoviedb.org/3/authentication", options)
-     
-        .then(response => {
-            console.log(response); 
-            return response.json();
-        })
-
-        .then(data => {
-            const movieList = document.getElementById("movieList");
-            movieList.innerHTML = `<p>${data.value}</p>`
-        })
-
-        .catch(error => console.log(error))
+    fetch(api_url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
 }
+getMovies()
 
 
 
-  
-  
