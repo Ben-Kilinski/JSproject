54c3748fb6c331658894f116c49c75770e99c27e
Ben-Kilinski <ben.kilinski@gmail.com>
2023-10-03 14:07:32 +0300

    home page

--- main.js
+++ main.js
@@ -6,15 +6,32 @@ const options = {
     }
   };
 
-const api_url = "https://www.themoviedb.org/movieapi_key=3141d64e429db4ab41c0554a80f4090f"
+const api_url = "https://api.themoviedb.org/3/trending/movie/day?language=en-US&page=1"
+
+const popMovies = document.getElementById("popMovies")
 
 function getMovies() {
-    fetch(api_url)
+    fetch(api_url, options)
         .then(res => res.json())
-        .then(data => console.log(data))
+
+        .then(data => {console.log(data)
+            
+            data.results.forEach(data => {
+                popMovies.innerHTML += 
+            `<div class="homeMovies">
+            <img src="http://image.tmdb.org/t/p/w500${data.poster_path}" style="width: 250px; height: 350px; border: 2px solid black;">    
+            ${data.title}
+                <img>
+            </div>`
+            }); 
+        })
+
         .catch(error => console.log(error));
 }
 getMovies()
+
+//----------------------pagination------------------------------------------
+
 
 
 
