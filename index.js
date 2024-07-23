66b0faf0bfdd5485c6ef400166ea3abd82c7fdf0
Ben Kilinski <ben.kilinski@gmail.com>
2024-07-23 22:18:57 +0300

    boolean expressions

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 //IITC 23.07.2024 - Home work
 
 //                       Task 1: Variable Declaration and Boolean Expressions
+
 //1 - Declare Variables
 //Declare two variables, firstName and lastName, and assign them your first and last name as string values.
-let firstName = "Ben";
+let firstName = "John";
 let lastNmae = "Kilinski";
 //Declare a variable age and assign it your age as a number.
 let age = 7;
 //Declare a variable isStudent and assign it a boolean value (true if you are a student, false otherwise).
 let isStudent = true;
+
 //2 - Boolean Expressions
 //Write a boolean expression to check if age is greater than 18. Assign the result to a variable isAdult.
 let isAgeGreaterThan18 = age >= 18
-
 if (isAgeGreaterThan18) console.log("You're an adult")
 else console.log("You're a teenager")
 //Write a boolean expression to check if firstName is equal to "John". Assign the result to a variable isJohn.
 let isFirstName = firstName === "John";
-if (isFirstName) console.log()
+if (isFirstName) console.log("Hello John"); else console.log("You're not John")
 
 
 
