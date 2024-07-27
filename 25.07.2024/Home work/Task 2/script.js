4dae9488bea767cbe2de0a68f241724f814cd9c6
Ben-Kilinski <ben.kilinski@gmail.com>
2024-07-28 00:05:34 +0300

    task 2

--- 25.07.2024/Home work/Task 2/script.js
+++ 25.07.2024/Home work/Task 2/script.js
@@ -5,3 +5,118 @@
 // string is declared betwen " " 
 
 //3
+//undefined is a variable declared (so it exists) but witout atributed value (not defined).
+//null is a value defined as zero, null or any value that dosen't make sense for the JS language.
+
+//4
+//using the typeof operator.
+
+//5
+//its a flooting number (2.5)
+
+//6
+//boolean (true or false)
+
+//7
+// trought that sintax:
+// let/const objetctName =  {
+//  propertie: "string",
+//  propertie: number 
+// } 
+
+//8
+//NaN (not a number) is a number data type
+
+//9
+// = assigne a value
+// == comparation (returns true or false) after coverting the values to a commom type
+// === comparation without converting the values to a commom type
+
+//10
+// Number() Function
+// parseInt() function
+// parseFloat() function
+// Unary Plus (+) Operator
+
+//11
+// In JavaScript, when you use the + operator between a string and a number, the number is converted to a string, and the two strings are concatenated.
+
+//12
+// let value = "Hello";
+//  if (typeof value === 'string') {
+//  console.log("It's a string!");
+//  }
+
+
+//13
+// template literals (tilda): ``
+//        `
+//         aefve
+//         efvefverfqerfq
+//         wrfqwrfqw
+//        `
+
+//14
+//single and double quotes are made for defining strings.
+//backsticks allows you to create multi-line strings 
+// and turn possible to the code to read variables inside a string with ${}
+
+//15 
+// string() function
+// toString() function
+// template lierals: `${num}`
+
+//16
+//console.log(typeof null) is an object
+
+//17
+// const variableName = "value"
+
+//18
+// the number is coerced to a string 
+//let num = 1;
+//let str = "The number is ";
+//let result = str + num;
+//console.log(result); // "The number is 5" (STRING)
+
+//19
+//boolean 
+
+//20
+// escaping double cotes(""): \"
+// escaping single cotes(''): \'
+// escaping backslash(\): \
+
+//21
+// var - global scope
+// let - block scope 
+// const - permanent value (cannot be reassigned after their initial assignment)
+
+//22
+// let variable;
+// if (typeof variable === 'undefined') {
+//    console.log('The variable is undefined.');
+//  }
+
+//23
+// string "105"
+
+//24
+// literal boolean - let student = true
+// boolean expression - let isEqual (4 === 4)
+
+//25
+// primitive data type is a single value (string, number, boolean...)
+// Objects are collections of properties
+
+
+  
+
+//16
+
+
+//21
+//var - 
+//let -
+//const - 
+
