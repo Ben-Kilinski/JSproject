97ff384826f0341f69482304e4edbaa3e8bbaa46
Ben-Kilinski <ben.kilinski@gmail.com>
2024-07-28 13:37:16 +0300

    task 4

---
+++ 25.07.2024/Home work/task4/script.js
@@ -0,0 +1,7 @@
+//-------------------Logical operators------------------------------//
+
+//1. What are the three main logical operators in JavaScript?
+//Hint: Think about AND, OR, and NOT.
+//and &
+//or ||
+//not !
