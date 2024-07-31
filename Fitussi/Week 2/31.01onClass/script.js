3a8370d972b54cc6fc28003064be300a4d3bf1e6
Ben-Kilinski <ben.kilinski@gmail.com>
2024-07-31 13:58:18 +0300

    objectsFitussi1.2

--- Fitussi/Week 2/31.01onClass/script.js
+++ Fitussi/Week 2/31.01onClass/script.js
@@ -32,3 +32,42 @@ console.log(`Company: ${car.company}
 Model: ${car.model}`)
 car.year = 2015
 console.log(car)
+
+let fruit = {
+    name: "strawberrie",
+    color: "red",
+    sweetness: 7
+}
+console.log(`${fruit.name}
+sweetness: ${fruit.sweetness}`)
+fruit.color = "blue"
+console.log(fruit)
+
+
+let book1 = {
+    title: "The lord of the Rings",
+    author: "Tolkien",
+    pages: 1227,
+}
+console.log(`${book1.title}
+Author: ${book1.author}`)
+book1.pages += 50
+console.log(book1)
+
+let book2 = {
+    title: "D'Vince Code",
+    author: "Dan Brown",
+    pages: 566,
+}
+
+let animal = {
+    species: "fish",
+    sound: "glu",
+    isWild: true,
+}
+console.log(`${animal.species}
+Author: ${animal.sound}`)
+animal.isWild = false
+console.log(animal)
+
+
