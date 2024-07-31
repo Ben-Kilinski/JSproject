7f1d6dff9b8584e6a296ebac8ea01d4e93bedacd
Ben-Kilinski <ben.kilinski@gmail.com>
2024-07-31 13:30:08 +0300

    objects.fitoussi

---
+++ Fitussi/Week 2/31.01onClass/script.js
@@ -0,0 +1,34 @@
+let spiderman = {
+    name: "Peter Parker",
+    age: 34,
+    profession: "photographer",
+    powers: "spider"
+}
+console.log(spiderman)
+
+let myComputer = {
+    company: "asus",
+    Ram: "16 Gb",
+    Hd: "1T",
+    video: "Nvidia",
+}
+console.log(myComputer)
+
+let person = {
+    name: "Ben",
+    age: 27,
+    isStudent: false,
+}
+person.isStudent = true
+console.log(`Name: ${person.name} 
+age: ${person.age}`)
+
+let car = {
+    company: "Peaugeaut",
+    model: 2008,
+    year: 2014
+}
+console.log(`Company: ${car.company}
+Model: ${car.model}`)
+car.year = 2015
+console.log(car)
