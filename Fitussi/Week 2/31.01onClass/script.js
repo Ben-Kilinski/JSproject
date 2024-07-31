557e10a7bb723b1a11a3abf29c2621e863252827
Ben-Kilinski <ben.kilinski@gmail.com>
2024-07-31 15:48:45 +0300

    Object.functionAnonymous

--- Fitussi/Week 2/31.01onClass/script.js
+++ Fitussi/Week 2/31.01onClass/script.js
@@ -1,4 +1,4 @@
-let spiderman = {
+/*let spiderman = {
     name: "Peter Parker",
     age: 34,
     profession: "photographer",
@@ -67,7 +67,36 @@ let animal = {
 }
 console.log(`${animal.species}
 Author: ${animal.sound}`)
-animal.isWild = false
-console.log(animal)
+animal.isWild = true
+console.log(animal)*/
 
+/*console.log(car.model)
+car.year = 2015
+car.color = "gray"
+console.log(car)*/
+
+let car = {
+    company: "Peugeot",
+    model: 2008,
+    year: 2014,
+    details: function () {
+        console.log(`details of the this car: ${this.company} (${this.model}), ${this.year}`)
+    }
+}
+car.details()
+
+let Keys_Array = Object.keys(car) // ['company', 'model', 'year', 'color']
+function printCars() {
+    for (let i = 0; i < Keys_Array.length; i++) {
+        console.log(`${Keys_Array[i]}: ${car[Keys_Array[i]]}`)
+    }
+}
+printCars(car)
+
+let person = {
+    name: "Ben",
+    age: 27,
+    isStudent: false,
+}
+console.log(Object.keys(person))
 
