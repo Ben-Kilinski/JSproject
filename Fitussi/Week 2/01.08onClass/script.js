6d7adb48167931985c95317b177d5a50fa368176
Ben-Kilinski <ben.kilinski@gmail.com>
2024-08-01 10:25:13 +0300

    Multiplication table

--- Fitussi/Week 2/01.08onClass/script.js
+++ Fitussi/Week 2/01.08onClass/script.js
@@ -7,3 +7,12 @@ for (let i = 1; i <= 5; i++) {
     }
     console.log("")
 }
+
+//Multiplication table
+for (let i = 1; i <= 10; i++) {
+    let row = "" // create 10 lines(rows)
+    for (let j = 1; j <= 10; j++) {
+        row += `${i * j} ` // adds results for the lines
+    }
+    console.log(row)
+}
