66f456c86f7b77c426089d0bf72e9e762aa4bf9d
Ben-Kilinski <ben.kilinski@gmail.com>
2024-08-07 10:22:07 +0300

    clean code moorning

--- Clean Code/script.js
+++ Clean Code/script.js
@@ -5,3 +5,19 @@ function max(x, y) {
     return Math.max(...numsAray)
 }
 console.log(max(x, y))
+
+let str = "hello word"
+function firstChar(str) {
+    return str.charAt(0)
+}
+console.log(firstChar(str))
+
+function roundToNearestInt(num) {
+    return Math.round(num)
+}
+console.log(roundToNearestInt(2.4))
+
+function startsWithSubstring(str, substring) {
+    return str.startsWith(substring)
+}
+console.log(startsWithSubstring(str, "helo"))
