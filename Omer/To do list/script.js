3660ad934d15d8e483469ad1c4aa44b7a9dbd69e
Ben Kilinski <ben.kilinski@gmail.com>
2024-09-19 14:02:09 +0300

    õug

--- Omer/To do list/script.js
+++ Omer/To do list/script.js
@@ -1,14 +1,20 @@
 const tasks_list = document.getElementById("tasks_list");
 const input = document.getElementById("input");
-const btn = document.getElementById("btn");
+const btn_sub = document.getElementById("btn");
+const btn_del = document.getElementById(`delete${count}`);
 
-btn.addEventListener("click", add_to_tasks);
+btn_sub.addEventListener("click", add_to_tasks);
 
 let count = 0;
-function add_to_tasks(){
+function add_to_tasks() {
     count += 1;
-    tasks_list.innerHTML += `
-        <li id="task${count}">👉task${count}: ${input.value}</li>`;
-    return count;
-    input.value= "";
+    tasks_list.innerHTML +=
+        `
+            <li id="task${count}">
+            👉task${count}: ${input.value}
+            <button id="delete${count}">delete</button>
+            </li>
+        `;
+    input.value = "";
 }
+
