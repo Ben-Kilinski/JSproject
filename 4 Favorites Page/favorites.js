117f05c063e05ac1442aedc4afec5f3f45edd7e9
Ben Kilinski <ben.kilinski@gmail.com>
2024-10-23 11:33:21 +0300

    well done

--- 4 Favorites Page/favorites.js
+++ 4 Favorites Page/favorites.js
@@ -29,29 +29,29 @@ function removeFromFavorites(movieId) {
 if (favorites.length === 0) {
     favoritesList.innerHTML = "<p>Nenhum filme nos favoritos</p>";
 } else {
-    let htmlContent = ''; // Variável para armazenar o conteúdo HTML
-
-    favorites.forEach(movieId => {
+    let promises = favorites.map(movieId => {
         const api_urlMovie = `https://api.themoviedb.org/3/movie/${movieId}?api_key=3141d64e429db4ab41c0554a80f4090f&language=en-US`;
 
-        // Faz a chamada para pegar os detalhes do filme
-        fetch(api_urlMovie, options)
+        // Faz a chamada para pegar os detalhes do filme e retorna uma promessa
+        return fetch(api_urlMovie, options)
             .then(res => res.json())
             .then(movieData => {
                 // Constrói o HTML para cada filme com o botão "Remove"
-                htmlContent += `
+                return `
                     <div class="favorite-movie" id="movie-${movieId}">
-                        <img src="http://image.tmdb.org/t/p/w500${movieData.poster_path}" alt="${movieData.title}">
-                        <h2>${movieData.title}</h2>
-                        <p>${movieData.overview}</p>
-                        <p>Release: ${movieData.release_date}</p>
-                        <button onclick="removeFromFavorites('${movieId}')">Remove</button>
+                        <img src="http://image.tmdb.org/t/p/w200${movieData.poster_path}" alt="${movieData.title}">
+                        <button class="remove-btn" onclick="removeFromFavorites('${movieId}')">Remove</button>
                     </div>
                 `;
-
-                // Atualiza o DOM apenas após todos os filmes serem processados
-                favoritesList.innerHTML = htmlContent;
-            })
-            .catch(error => console.log(error));
+            });
     });
+
+    // Aguarda todas as promessas serem resolvidas
+    Promise.all(promises)
+        .then(moviesHtml => {
+            // Atualiza o DOM uma única vez com todos os filmes
+            favoritesList.innerHTML = moviesHtml.join("");
+        })
+        .catch(error => console.log(error));
 }
+
