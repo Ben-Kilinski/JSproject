3e5db555ccf5115724b266b956babc3e5d99d141
Ben Kilinski <ben.kilinski@gmail.com>
2024-11-18 15:15:26 +0200

    layout done 3

--- Fitussi/after lebanon/18-11-24/vite-project/src/App.jsx
+++ Fitussi/after lebanon/18-11-24/vite-project/src/App.jsx
@@ -2,35 +2,37 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Button from './components/Button.jsx'
+// import Button from './components/Button.jsx'
 // import Card from './components/Wrapper.jsx'
-import DisplayCards from './components/Model.jsx'
+// import DisplayCards from './components/Model.jsx'
+import Layout from './components/Layout.jsx'
 
 function App() {
 
+  //2 Model
+  // const [isOpen, setIsOpen] = useState(false);
 
-  const [isOpen, setIsOpen] = useState(false);
+  // const handleOpenModal = () => {
+  //   setIsOpen(true);
+  // };
 
-  const handleOpenModal = () => {
-    setIsOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsOpen(false);
-  };
+  // const handleCloseModal = () => {
+  //   setIsOpen(false);
+  // };
 
 
 
 
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {
+      /* <div>
+      <a href="https://vite.dev" target="_blank">
+      <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+      <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -50,20 +52,25 @@ function App() {
       {/* <div>
         <h1>this is h1</h1>
         <Card>
-          <h2>h2 inside card</h2>
-          <p>p inside card</p>
+        <h2>h2 inside card</h2>
+        <p>p inside card</p>
         </Card>
-      </div> */}
+        </div> */}
 
       {/* //2)Modal component  */}
 
-      <div>
+      {/* <div>
         <button onClick={handleOpenModal}>Open Modal</button>
         <DisplayCards isOpen={isOpen}>
         <h2>It´s me, Modal!</h2>
         <p>:D</p>
-          <button onClick={handleCloseModal}>Close Modal</button>
+        <button onClick={handleCloseModal}>Close Modal</button>
         </DisplayCards>
+      </div> */}
+
+      {/* 3. Layout Component */}
+      <div>
+        <Layout/>
       </div>
 
     </>
