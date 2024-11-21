13016075a3eabf711edf0b50665877c62991e311
Ben Kilinski <ben.kilinski@gmail.com>
2024-11-21 12:00:12 +0200

    mui

--- Fitussi/Example/react-route-dom/src/App.jsx
+++ Fitussi/Example/react-route-dom/src/App.jsx
@@ -9,6 +9,7 @@ import Home from "./Pages/Home/Home.jsx"
 import About from "./Pages/About/About.jsx"
 import Dashboard from "./Pages/Dashboard/Dashboard.jsx"
 import Articles from "./Pages/Articles/Articles.jsx"
+import Navbar from './Components/Navbar.jsx'
 
 
 
@@ -16,14 +17,15 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/Adashboard" element={<Dashboard />} />
-          <Route path="/Articles" element={<Articles />} >
-            <Route path="News" element={<h2>This is News Section</h2>} />
-            <Route path="Politics" element={<h2>This is Politics Section</h2>} />
-            <Route path="Tech" element={<h2>This is Tech Section</h2>} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/articles" element={<Articles />} >
+            <Route path="news" element={<h2>This is News Section</h2>} />
+            <Route path="politics" element={<h2>This is Politics Section</h2>} />
+            <Route path="tech" element={<h2>This is Tech Section</h2>} />
           </Route>
         </Routes>
       </BrowserRouter>
