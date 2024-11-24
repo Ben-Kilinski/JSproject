5f8e8f5873cbf3367f3b739334989dccf36a3073
Ben Kilinski <ben.kilinski@gmail.com>
2024-11-24 09:51:28 +0200

    Atualiza .gitignore e remove arquivos irrelevantes

--- Fitussi/Pokemon/pokemon/src/App.js
+++ Fitussi/Pokemon/pokemon/src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import axios from "axios"; 
 
+//import react hooks useState (to storage pokemons) and 
+// useEffect(to search for data after the page is loaded) 
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  
-  const [pokemonList, setPokemonList] = useState([]); 
+  const [pokemonList, setPokemonList] = useState([]);
 
-  useEffect( // 'efeitos' a serem feitos após o react montar a tela 
-    () => {console.log("o hook useEffect foi usado!")}, // o que fazer 
-    [] // quando fazer 
-  );
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=20'); //API requisition
+        const data = await response.json();//api converted to Json format (readible to JS)
+        setPokemonList(data.results); // Storage the data at the state pokemonlist
+        console.log(data.results); // Exibindo a lista no console
+      } catch (error) {
+        console.error('Erro ao buscar Pokémon:', error);
+      }
+    };
+
+    fetchPokemon();
+  }, []);
 
   return (
     <div className="App">
       <h1>Pokémon Viewer App</h1>
-      <p>Show time!</p>
     </div>
   );
 }
